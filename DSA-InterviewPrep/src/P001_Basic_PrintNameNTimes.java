1570a2be392444e2dd930ed7673734b269cfcee6
Amul Op <amulsharmarr67@gmail.com>
2025-11-09 15:40:57 +0530

    Recursion - Basic - printNameNTimes

---
+++ DSA-InterviewPrep/src/P001_Basic_PrintNameNTimes.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class P001_Basic_PrintNameNTimes {
+     public static void main(String[] args){
+          //demo call from here to check it out
+          printNameNTimes(5);
+     }
+
+     //function to print the name n times using recursion
+     private static void  printNameNTimes(int n){
+          if(n == 0)     return;
+          System.out.println("Amul Sharma");
+          printNameNTimes(n-1);
+     }
+}
