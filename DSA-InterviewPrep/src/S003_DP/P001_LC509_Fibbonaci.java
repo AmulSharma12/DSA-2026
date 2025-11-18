0d4e0df47e6b0cf6460788b56cb2cdc6df78edb4
Amul Op <amulsharmarr67@gmail.com>
2025-11-18 17:24:10 +0530

    DP - LC509 - Fibbonaci

--- DSA-InterviewPrep/src/S003_DP/P001_LC509_Fibbonaci.java
+++ DSA-InterviewPrep/src/S003_DP/P001_LC509_Fibbonaci.java
@@ -1,4 +1,5 @@
 package S003_DP;
+import java.util.Arrays;
 
 public class P001_LC509_Fibbonaci {
     public static void main(String[] args){
@@ -11,5 +12,17 @@ public class P001_LC509_Fibbonaci {
         return fib(n - 1) + fib(n - 2);
     }
 
+    //Memoize way
+    public int fibMemoize(int n) {
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        return fibbonaci(n, dp);
+    }
 
+    private int fibbonaci(int n , int[] dp){
+        if(n <= 1)   return n;
+        if(dp[n] != -1)   return dp[n];
+        System.out.println(n + "----------");
+        return dp[n] =  fibbonaci(n-1, dp) + fibbonaci(n -2 , dp);
+    }
 }
