d2357878e5b63bdaafdbcb5b95323c95588bab3a
Amul Op <amulsharmarr67@gmail.com>
2025-11-18 17:49:28 +0530

    DP - LC509 - Fibbonaci tabulation and space optimization

--- DSA-InterviewPrep/src/S003_DP/P001_LC509_Fibbonaci.java
+++ DSA-InterviewPrep/src/S003_DP/P001_LC509_Fibbonaci.java
@@ -25,4 +25,32 @@ public class P001_LC509_Fibbonaci {
         System.out.println(n + "----------");
         return dp[n] =  fibbonaci(n-1, dp) + fibbonaci(n -2 , dp);
     }
+
+
+    //Tabulation way
+    public int fibWithTabulation(int n) {
+        if(n <= 1)  return n;
+        int[] dp = new int[n+1];
+
+        dp[0] = 0; dp[1] = 1;
+        for(int i = 2; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
+    }
+
+    //space optimization way
+    public int fibWithSpaceOptimization(int n) {
+        if(n<=1) return n;
+        int prev2 = 0;
+        int prev = 1;
+
+        for(int i = 2; i<=n; i++){
+            int curr = prev + prev2;
+            prev2 = prev;
+            prev = curr;
+        }
+        return prev;
+    }
 }
