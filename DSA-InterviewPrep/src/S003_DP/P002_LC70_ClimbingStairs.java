70a9a634394a5ca0d307c555ba82d1184aaf2a18
Amul Op <amulsharmarr67@gmail.com>
2025-11-19 23:15:49 +0530

    DP - LC70 - Climbing Stairs - Memoization

--- DSA-InterviewPrep/src/S003_DP/P002_LC70_ClimbingStairs.java
+++ DSA-InterviewPrep/src/S003_DP/P002_LC70_ClimbingStairs.java
@@ -1,5 +1,5 @@
 package S003_DP;
-
+import java.util.Arrays;
 public class P002_LC70_ClimbingStairs {
     public static void main(String[] args){
         //call from here..
@@ -14,5 +14,23 @@ public class P002_LC70_ClimbingStairs {
         int twoStep = climbStairsRecursive(n - 2);
 
         return oneStep + twoStep;
+    }
+
+    //Memoize way
+    public static int climbStairsM(int n) {
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        return climbStairsUsingMemoization(n, dp);
+    }
+
+    public static int climbStairsUsingMemoization(int n,int[] dp) {
+        if(n == 0)  return 1;
+        if(n < 0)   return 0;
+
+        if(dp[n] != -1)  return dp[n];
+        int oneStep = climbStairsUsingMemoization(n - 1, dp);
+        int twoStep = climbStairsUsingMemoization(n - 2, dp);
+
+        return dp[n] = oneStep + twoStep;
     }
 }
