b19d1af6d47ebf2612c21d1107dba531e5a8213b
Amul Op <amulsharmarr67@gmail.com>
2025-11-19 23:22:16 +0530

    DP - LC70 - Climbing Stairs - Memoizationcls

--- DSA-InterviewPrep/src/S003_DP/P002_LC70_ClimbingStairs.java
+++ DSA-InterviewPrep/src/S003_DP/P002_LC70_ClimbingStairs.java
@@ -33,4 +33,16 @@ public class P002_LC70_ClimbingStairs {
 
         return dp[n] = oneStep + twoStep;
     }
+
+
+    public static int climbStairsT(int n) {
+        if(n == 1) return 1;
+        int[] dp = new int[n+1];
+        dp[0] = dp[1] =  1;
+        for(int i = 2; i<=n; i++){
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
 }
