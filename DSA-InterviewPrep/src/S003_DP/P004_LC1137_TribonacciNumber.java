f976877df011ff89bb77ed07d6402f5aaf8db03d
Amul Op <amulsharmarr67@gmail.com>
2025-11-22 23:35:28 +0530

    DP - LC1137 - Tribonacci Number - Space Optimization

--- DSA-InterviewPrep/src/S003_DP/P004_LC1137_TribonacciNumber.java
+++ DSA-InterviewPrep/src/S003_DP/P004_LC1137_TribonacciNumber.java
@@ -45,4 +45,23 @@ public class P004_LC1137_TribonacciNumber {
 
         return dp[n];
     }
+
+    //Space Optimization solution
+    private static int tribonacciUsingSO(int n){
+        if(n == 0)  return 0;
+        if(n <= 2)  return 1;
+
+        int prev = 1;
+        int prev2 = 1;
+        int prev3 = 0;
+
+        for(int i = 3; i<=n; i++){
+            int curr = prev + prev2 + prev3;
+            prev3 = prev2;
+            prev2 = prev;
+            prev = curr;
+        }
+
+        return prev;
+    }
 }
