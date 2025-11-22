4d6d9ff3c37428c3438b77f210e6ef085568a8e1
Amul Op <amulsharmarr67@gmail.com>
2025-11-22 23:17:46 +0530

    DP - LC1137 - Tribonnaci Number -Memoization solution

--- DSA-InterviewPrep/src/S003_DP/P004_LC1137_TribonacciNumber.java
+++ DSA-InterviewPrep/src/S003_DP/P004_LC1137_TribonacciNumber.java
@@ -1,5 +1,7 @@
 package S003_DP;
 
+import java.util.Arrays;
+
 public class P004_LC1137_TribonacciNumber {
     public static void main(String[] args){
         //call from here...
@@ -10,5 +12,22 @@ public class P004_LC1137_TribonacciNumber {
         if(n == 0)   return 0;
         if(n <= 2)  return 1;
         return tribonacciR(n-1) + tribonacciR(n - 2) + tribonacciR(n - 3);
+    }
+
+    //Memoization solution
+    public int tribonacciMemoize(int n) {
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, - 1);
+        return tribonacciM(n, dp);
+    }
+
+    private int tribonacciM(int n, int[] dp)
+    {
+        if(n == 0)  return 0;
+        if(n <= 2)  return 1;
+
+        if(dp[n] != - 1)    return dp[n];
+
+        return  dp[n] = tribonacciM(n - 1, dp) + tribonacciM(n - 2, dp) + tribonacciM(n - 3, dp);
     }
 }
