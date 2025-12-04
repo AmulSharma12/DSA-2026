592bbe2d1de7701148d8c69b209b99ddeefa1e52
Amul Op <amulsharmarr67@gmail.com>
2025-12-04 22:27:22 +0530

    DP - LC198 - House Robber - Memoization solution

--- DSA-InterviewPrep/src/S003_DP/P007_LC198_HouseRobber.java
+++ DSA-InterviewPrep/src/S003_DP/P007_LC198_HouseRobber.java
@@ -1,4 +1,5 @@
 package S003_DP;
+import java.util.Arrays;
 
 public class P007_LC198_HouseRobber {
     public static void main(String[] args){
@@ -18,6 +19,24 @@ public class P007_LC198_HouseRobber {
         int moneyPicked = moneyStreet[position] + maxRobbingR(position -2, moneyStreet);
         int moneyNotPicked = 0 + maxRobbingR(position-1, moneyStreet);
         return Math.max(moneyPicked, moneyNotPicked);
+    }
+
+    //Memoization solution
+    public static int rob(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        return maxRobbingM(n-1, nums, dp);
+    }
+
+    private static int maxRobbingM(int position, int[] moneyStreet,int[] dp){
+        if(position == 0)   return moneyStreet[position];
+        if(position < 0)    return 0;
+        if(dp[position] != -1)  return dp[position];
+
+        int moneyPicked = moneyStreet[position] + maxRobbingM(position -2, moneyStreet, dp);
+        int moneyNotPicked = 0 + maxRobbingM(position-1, moneyStreet, dp);
+        return dp[position] = Math.max(moneyPicked, moneyNotPicked);
     }
 
 }
