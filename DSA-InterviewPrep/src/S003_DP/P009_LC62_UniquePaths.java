7e2039c866028bd6d012f52f018a211912936e55
Amul Op <amulsharmarr67@gmail.com>
2025-12-14 16:42:55 +0530

    DP - LC62 - UniquePaths

--- DSA-InterviewPrep/src/S003_DP/P009_LC62_UniquePaths.java
+++ DSA-InterviewPrep/src/S003_DP/P009_LC62_UniquePaths.java
@@ -43,4 +43,25 @@ public class P009_LC62_UniquePaths {
 
         return cache[row][col] = upDirectionPaths + leftDirectionPaths;
     }
+
+    //Tabulation solution
+    private int uniquePathsT(int m, int n){
+        int[][] dp = new int[m][n];
+
+
+        for(int row = 0 ; row < m ; row++){
+            for(int col = 0; col < n; col++){
+                if(row == 0 && col == 0)    dp[0][0] = 1;
+                else{
+                    int up = 0;
+                    int left = 0;
+                    if(row > 0) up = dp[row-1][col];
+                    if(col > 0) left = dp[row][col-1];
+                    dp[row][col] = up + left;
+                }
+            }
+        }
+
+        return dp[m-1][n-1];
+    }
 }
