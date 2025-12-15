04df2b1a96549f7fd0ce740e0c44796f391cae1d
Amul Op <amulsharmarr67@gmail.com>
2025-12-15 22:23:27 +0530

    DP - LC63 - UniquePaths2 memoization solution

--- DSA-InterviewPrep/src/S003_DP/P010_LC63_UniquePaths2.java
+++ DSA-InterviewPrep/src/S003_DP/P010_LC63_UniquePaths2.java
@@ -1,10 +1,12 @@
 package S003_DP;
+import java.util.Arrays;
 
 public class P010_LC63_UniquePaths2 {
     public static void main(String[] args){
         //call from here...
     }
-
+    
+    //Recursive solution
     public static int uniquePathsWithObstaclesR(int[][] obstacleGrid) {
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
@@ -24,5 +26,31 @@ public class P010_LC63_UniquePaths2 {
 
         //sum up all
         return up + left;
+    }
+
+    //Memoization solution
+    public static int uniquePathsWithObstaclesM(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] dp = new int[m][n];
+        for(int[] dpRow: dp){
+            Arrays.fill(dpRow,-1);
+        }
+        return findUniquePathsM(m-1, n-1, obstacleGrid, dp);
+    }
+
+    private static int findUniquePathsM(int row, int col, int[][] grid, int[][] dp){
+        //base cases
+        if(row < 0 || col < 0)  return 0;
+        if(grid[row][col] == 1) return 0;
+        if(row == 0 && col == 0)    return 1;
+        if(dp[row][col] != -1)  return dp[row][col];
+
+        //recursive calls
+        int up = findUniquePathsM(row-1, col, grid,dp);
+        int left = findUniquePathsM(row, col-1, grid,dp);
+
+        //sum up all
+        return dp[row][col] = up + left;
     }
 }
