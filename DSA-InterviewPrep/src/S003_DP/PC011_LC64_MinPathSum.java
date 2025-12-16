b8dd79feed9bb546ecfb28ac1c52ee424748560a
Amul Op <amulsharmarr67@gmail.com>
2025-12-17 00:11:05 +0530

    DP - LC64 - MinPath sum tabulation solution

--- DSA-InterviewPrep/src/S003_DP/PC011_LC64_MinPathSum.java
+++ DSA-InterviewPrep/src/S003_DP/PC011_LC64_MinPathSum.java
@@ -56,4 +56,33 @@ public class PC011_LC64_MinPathSum {
         return  dp[row][col] = grid[row][col] + Math.min(upDirectionPathSum, leftDirectionPathSum);
     }
 
+    //Tabulation solution
+    private static int minPathSumTabulation(int[][] grid){
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+
+        dp[0][0] = grid[0][0];
+
+        for(int row = 0; row < m; row++){
+            for(int col = 0; col < n; col++){
+                if(!(row == 0 && col == 0)) {
+                    // int upDirPathSum = Integer.MAX_VALUE;
+                    // int leftDirPathSum = Integer.MAX_VALUE;
+                    // if(row > 0) upDirPathSum = dp[row-1][col];
+                    // if(col > 0) leftDirPathSum = dp[row][col-1];
+                    dp[row][col] =
+                            grid[row][col] +
+                                    Math.min(
+                                            row > 0? dp[row-1][col]: Integer.MAX_VALUE,
+                                            col > 0? dp[row][col-1] : Integer.MAX_VALUE
+                                    );
+                }
+
+            }
+        }
+
+        return dp[m-1][n-1];
+    }
+
 }
