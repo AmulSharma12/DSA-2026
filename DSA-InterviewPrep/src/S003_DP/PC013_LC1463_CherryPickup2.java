0f91fd589c7a9729fdc78e4a1c53905a4d3ae304
Amul Op <amulsharmarr67@gmail.com>
2026-01-04 21:53:10 +0530

    DP - LC1463 - Cherry Pickup 2 Recursive solution

---
+++ DSA-InterviewPrep/src/S003_DP/PC013_LC1463_CherryPickup2.java
@@ -0,0 +1,35 @@
+package S003_DP;
+
+public class PC013_LC1463_CherryPickup2 {
+    public static void main(String[] args){
+        //call from here...
+    }
+
+    //Recursive solution
+    private static int cherryPickupRecursive(int row, int col1, int col2, int m, int n, int[][] grid){
+        if(col1 < 0 || col2 < 0 || col1 >= n || col2 >= n) return Integer.MIN_VALUE;
+        if(row == m-1) {
+            if(col1==col2)  return grid[row][col1];
+            return grid[row][col1] + grid[row][col2];
+        }
+
+        int maxi = Integer.MIN_VALUE;
+        for(int dj1 = -1; dj1 <= 1 ; dj1++){
+            for(int dj2 = -1; dj2 <= 1; dj2++){
+                int value = 0;
+                if(col1==col2)  value =  grid[row][col1];
+                else value = grid[row][col1] + grid[row][col2];
+                value += cherryPickupRecursive(row+1, col1+dj1, col2+dj2, m, n, grid);
+                maxi = Math.max(maxi, value);
+            }
+        }
+
+        return maxi;
+    }
+
+    public static int cherryPickupR(int[][] grid) {
+        int rowLen = grid.length;
+        int colLen = grid[0].length;
+        return cherryPickupRecursive(0, 0, colLen-1, rowLen, colLen, grid);
+    }
+}
