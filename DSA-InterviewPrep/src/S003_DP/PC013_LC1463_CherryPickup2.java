58a662b2a097ebffd17d61c36f963d611615460a
Amul Op <amulsharmarr67@gmail.com>
2026-01-06 09:26:23 +0530

    DP - LC1463 - Cherry pickup 2 tabulation solution

--- DSA-InterviewPrep/src/S003_DP/PC013_LC1463_CherryPickup2.java
+++ DSA-InterviewPrep/src/S003_DP/PC013_LC1463_CherryPickup2.java
@@ -69,4 +69,47 @@ public class PC013_LC1463_CherryPickup2 {
         }
         return cherryPickupMemoization(0, 0, colLen-1, rowLen, colLen, grid, dp);
     }
+
+    //Tabulation solution
+    public int cherryPickupT(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        int[][][] dp = new int[n][m][m];
+
+        //base case states last row - having all the col1/col2 moving from 0 to m-1
+        for(int j1 = 0; j1 < m; j1++){
+            for(int j2 = 0; j2 < m; j2++){
+                if(j1 == j2) dp[n-1][j1][j2] = grid[n-1][j1];
+                else dp[n-1][j1][j2] = grid[n-1][j1] + grid[n-1][j2];
+            }
+        }
+
+        //expressing all the state from n-2 to 0 each state have col moving from 0 to m-1
+        for(int i = n-2; i>=0; i--){
+            for(int j1 = 0; j1<m; j1++){
+                for(int j2 = 0; j2<m; j2++){
+
+                    //for particular state -computing maximum
+                    int maxi = Integer.MIN_VALUE;
+                    for(int dj1 = -1; dj1 <= 1; dj1++){
+                        for(int dj2 = -1; dj2 <= 1; dj2++){
+                            int value = 0;
+                            if(j1==j2)  value = grid[i][j1];
+                            else value = grid[i][j1] + grid[i][j2];
+                            if(j1+dj1 >= 0 && j1+dj1 < m && j2+dj2 >=0 && j2 + dj2 < m)
+                                value += dp[i+1][j1+dj1][j2+dj2];
+                            else value = Integer.MIN_VALUE;
+
+                            maxi = Math.max(maxi, value);
+                        }
+                    }
+
+                    dp[i][j1][j2] = maxi;
+                }
+            }
+        }
+
+        return dp[0][0][m-1];
+
+    }
 }
