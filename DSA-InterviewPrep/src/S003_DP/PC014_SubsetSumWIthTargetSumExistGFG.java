b12be2e8a3a06c925c7eb0ae55e68360ff116d6f
Amul Op <amulsharmarr67@gmail.com>
2026-01-08 23:32:07 +0530

    DP - 14 GFG Subset sum with target exist - Memoization solution

--- DSA-InterviewPrep/src/S003_DP/PC014_SubsetSumWIthTargetSumExistGFG.java
+++ DSA-InterviewPrep/src/S003_DP/PC014_SubsetSumWIthTargetSumExistGFG.java
@@ -1,11 +1,13 @@
 package S003_DP;
+import java.util.Arrays;
 //https://www.geeksforgeeks.org/problems/subset-sum-problem-1611555638/1
 public class PC014_SubsetSumWIthTargetSumExistGFG {
     public static void main(String[] args){
         //call from here...
     }
 
-    static Boolean isSubsetSum(int[] arr, int sum) {
+    //recursive solution
+    static Boolean isSubsetSumR(int[] arr, int sum) {
         int n = arr.length;
         return solveRecursive(n-1, arr, sum);
     }
@@ -23,5 +25,32 @@ public class PC014_SubsetSumWIthTargetSumExistGFG {
 
 
         return take || notTake;
+    }
+
+
+    static Boolean isSubsetSumM(int arr[], int sum) {
+        int n = arr.length;
+        int[][] dp = new int[n][sum+1];
+        for(int[] dpRow: dp){
+            Arrays.fill(dpRow,-1);
+        }
+        return solveMemoize(n-1, arr, sum, dp);
+    }
+
+    static Boolean solveMemoize(int ind, int[] nums, int target, int[][] dp){
+        if(target == 0) return true;
+        if(ind == 0 && nums[0] == target) return true;
+        if(ind < 0)   return false;
+
+        if(dp[ind][target] != -1)   return dp[ind][target] == 0 ? false : true;
+
+        boolean notTake;
+        notTake = solveMemoize(ind-1, nums, target, dp);
+
+        boolean take = false;
+        if(nums[ind] <= target) take = solveMemoize(ind-1, nums, target - nums[ind], dp);
+
+        dp[ind][target] = take || notTake ? 1 : 0;
+        return  take || notTake;
     }
 }
