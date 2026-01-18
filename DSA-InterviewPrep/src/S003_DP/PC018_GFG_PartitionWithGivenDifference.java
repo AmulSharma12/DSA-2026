fd6c99ebf40e67766d19155ab270226868435057
Amul Op <amulsharmarr67@gmail.com>
2026-01-18 18:20:20 +0530

    DP-18 GFG Partition With Given Difference - space optimization  solution

--- DSA-InterviewPrep/src/S003_DP/PC018_GFG_PartitionWithGivenDifference.java
+++ DSA-InterviewPrep/src/S003_DP/PC018_GFG_PartitionWithGivenDifference.java
@@ -105,4 +105,32 @@ public class PC018_GFG_PartitionWithGivenDifference {
         return dp[n-1][k];
     }
 
+
+    //Space Optimization solution
+    private static int countPartitionSO(int[] nums, int k){
+        int n = nums.length;
+        int[] prev = new int[k+1];
+
+        if(nums[0] <= k) prev[nums[0]] = 1;
+        if(nums[0] == 0) prev[0] = 2;
+        else prev[0] = 1;
+
+
+        for(int index = 1; index < n; index++){
+            int[] curr = new int[k+1];
+            for(int target = 0; target <= k ; target++){
+                int notTake = prev[target];
+
+                int take = 0;
+                if(nums[index] <= target)
+                    take = prev[target-nums[index]];
+
+                curr[target] = take + notTake;
+            }
+            prev = curr;
+        }
+
+        return prev[k];
+    }
+
 }
