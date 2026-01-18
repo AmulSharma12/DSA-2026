34e3f9d423ead12c44f0f03ef6aa90e11460f532
Amul Op <amulsharmarr67@gmail.com>
2026-01-18 22:50:23 +0530

    DP-19 GFG 0/1 knapsack memoization solution

--- DSA-InterviewPrep/src/S003_DP/PC019_GFG_01Knapsack.java
+++ DSA-InterviewPrep/src/S003_DP/PC019_GFG_01Knapsack.java
@@ -1,5 +1,5 @@
 package S003_DP;
-
+import java.util.Arrays;
 public class PC019_GFG_01Knapsack {
     public static void main(String[] args){
         //call from here...
@@ -24,5 +24,32 @@ public class PC019_GFG_01Knapsack {
             take = val[index] + knapsackRecursive(index-1, val, wt, W-wt[index]);
 
         return Math.max(take, notTake);
+    }
+
+    //Memoization solution
+    public static int knapsackM(int W, int val[], int wt[]) {
+        int n = val.length;
+        int[][] dp = new int[n][W+1];
+        for(int[] dpRow: dp){
+            Arrays.fill(dpRow,-1);
+        }
+        return knapsackMemoize(n-1, val, wt, W, dp);
+    }
+
+    private static int knapsackMemoize(int index, int[] val,int[] wt, int W,int[][] dp){
+        //base case
+        if(index == 0){
+            if(wt[0] <= W)   return val[0];
+            return 0;
+        }
+        if(dp[index][W] != -1)  return dp[index][W];
+
+
+        int notTake = 0 + knapsackMemoize(index-1, val, wt, W, dp);
+        int take = Integer.MIN_VALUE;
+        if(wt[index]  <= W)
+            take = val[index] + knapsackMemoize(index-1, val, wt, W-wt[index], dp);
+
+        return dp[index][W] = Math.max(take, notTake);
     }
 }
