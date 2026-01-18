3b50b80a2ced34d4cf22c40bf77a5d264f8da084
Amul Op <amulsharmarr67@gmail.com>
2026-01-18 18:08:33 +0530

    DP-18 GFG Partition With Given Difference - memoization solution

--- DSA-InterviewPrep/src/S003_DP/PC018_GFG_PartitionWithGivenDifference.java
+++ DSA-InterviewPrep/src/S003_DP/PC018_GFG_PartitionWithGivenDifference.java
@@ -1,4 +1,5 @@
 package S003_DP;
+import java.util.Arrays;
 //https://www.geeksforgeeks.org/problems/partitions-with-given-difference/1
 public class PC018_GFG_PartitionWithGivenDifference {
     public static void main(String[] args){
@@ -31,5 +32,38 @@ public class PC018_GFG_PartitionWithGivenDifference {
             take = countPartitionRecursiveR(index-1, nums, target-nums[index]);
 
         return take + notTake;
+    }
+
+    public int countPartitionsM(int[] arr, int diff) {
+        int n = arr.length;
+        int totalSum = 0;
+        for(int index = 0; index < n; index++) totalSum += arr[index];
+        if((totalSum - diff < 0) || (totalSum - diff) % 2 != 0) return 0;
+        int target = (totalSum-diff)/2;
+        int[][] dp = new int[n][target+1];
+        for(int[] dpRow: dp){
+            Arrays.fill(dpRow,-1);
+        }
+        return countPartitionMemoize(n-1, arr, target, dp);
+
+    }
+
+
+    private int countPartitionMemoize(int index, int[] nums, int target, int[][] dp){
+
+        if(index == 0){
+            if(target == 0 && nums[0] == 0)  return 2;
+            if(target == 0 || nums[0] == target)  return 1;
+            return 0;
+        }
+
+        if(dp[index][target] != -1) return dp[index][target];
+
+        int notTake = countPartitionMemoize(index-1, nums, target, dp);
+        int take = 0;
+        if(nums[index] <= target)
+            take = countPartitionMemoize(index-1, nums, target-nums[index], dp);
+
+        return dp[index][target] = take + notTake;
     }
 }
