095922de9236af0f320fb16624e2e658a2ca2689
Amul Op <amulsharmarr67@gmail.com>
2026-01-20 00:05:05 +0530

    DP-20 LC322 Coin change - memoization solution

--- DSA-InterviewPrep/src/S003_DP/PC020_LC322_CoinChange.java
+++ DSA-InterviewPrep/src/S003_DP/PC020_LC322_CoinChange.java
@@ -1,12 +1,14 @@
 package S003_DP;
+import java.util.Arrays;
 
+//https://leetcode.com/problems/coin-change/description/
 public class PC020_LC322_CoinChange {
     public static void main(String[] args){
         //call from here...
     }
 
     //Recursive solution
-    public static int coinChange(int[] coins, int amount) {
+    public static int coinChangeR(int[] coins, int amount) {
         int n = coins.length;
         if(amount == 0) return 0;
         int[][] dp = new int[n][amount+1];
@@ -31,5 +33,37 @@ public class PC020_LC322_CoinChange {
             take = 1 + coinChangeRecursive(index, amount-coins[index], coins);
 
         return Math.min(take ,notTake);
+    }
+
+    //Memoization solution
+    public int coinChangeM(int[] coins, int amount) {
+        int n = coins.length;
+        if(amount == 0) return 0;
+        int[][] dp = new int[n][amount+1];
+        for(int[] dpRow: dp){
+            Arrays.fill(dpRow,-1);
+        }
+        int ans = coinChangeMemoize(n-1, amount, coins, dp);
+        if(ans >= Integer.MAX_VALUE/2)  return -1;
+        return ans;
+    }
+
+    private int coinChangeMemoize(int index, int amount, int[] coins, int[][] dp){
+        //base case
+        if(index == 0){
+            if(amount % coins[0] == 0)
+                return amount/coins[0];
+            return Integer.MAX_VALUE/2;
+        }
+
+        if(dp[index][amount] != -1) return dp[index][amount];
+
+        //recursive function
+        int notTake = 0 + coinChangeMemoize(index-1, amount, coins, dp);
+        int take = Integer.MAX_VALUE;
+        if(coins[index] <= amount)
+            take = 1 + coinChangeMemoize(index, amount-coins[index], coins, dp);
+
+        return dp[index][amount] =  Math.min(take ,notTake);
     }
 }
