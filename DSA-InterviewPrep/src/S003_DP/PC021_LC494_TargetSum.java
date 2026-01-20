60283e140c5b3d9efd65a998bb63332b3a57d000
Amul Op <amulsharmarr67@gmail.com>
2026-01-21 00:11:25 +0530

    DP-21 LC494 Target sumn - memoization  solution

--- DSA-InterviewPrep/src/S003_DP/PC021_LC494_TargetSum.java
+++ DSA-InterviewPrep/src/S003_DP/PC021_LC494_TargetSum.java
@@ -33,4 +33,37 @@ public class PC021_LC494_TargetSum {
 
         return take + notTake;
     }
+
+    //Memoization solution
+    private int targetSumM(int[] nums, int target){
+        int n = nums.length;
+        int totalSum = 0;
+        for(int element: nums)  totalSum += element;
+        if((totalSum - target) < 0 || (totalSum - target)%2 != 0)   return 0;
+        int T = (totalSum - target)/2;
+        int[][] dp = new int[n][T+1];
+        for(int[] dpRow: dp){
+            Arrays.fill(dpRow,-1);
+        }
+        return targetSumMemoize(n-1, nums, T, dp);
+    }
+
+
+    private int targetSumMemoize(int index, int[] nums, int T, int[][] dp){
+        if(index == 0){
+            if(T == 0 && nums[0] == 0)    return 2;
+            if(T == 0) return 1;
+            if(nums[0] == T) return 1;
+            return 0;
+        }
+
+        if(dp[index][T] != -1)    return dp[index][T];
+
+        int notTake = targetSumMemoize(index-1, nums, T, dp);
+        int take = 0;
+        if(nums[index] <= T)
+            take = targetSumMemoize(index-1, nums, T-nums[index], dp);
+
+        return dp[index][T] = take + notTake;
+    }
 }
