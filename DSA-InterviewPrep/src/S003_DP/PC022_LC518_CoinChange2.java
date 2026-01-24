eab0a4369e01f406f4527a7c399d8f3c793d733d
Amul Op <amulsharmarr67@gmail.com>
2026-01-24 14:24:38 +0530

    DP-22 LC518 Coin change 2 - space optimization solution

--- DSA-InterviewPrep/src/S003_DP/PC022_LC518_CoinChange2.java
+++ DSA-InterviewPrep/src/S003_DP/PC022_LC518_CoinChange2.java
@@ -77,4 +77,30 @@ public class PC022_LC518_CoinChange2 {
 
         return dp[n-1][amount];
     }
+
+    //space optimization solution
+    private static int coinChangeSO(int amount, int[] coins){
+        int n = coins.length;
+        int[] prev = new int[amount+1];
+
+
+        for(int target = 0; target <= amount; target++){
+            if(target%coins[0] == 0) prev[target] = 1;
+        }
+
+        for(int index = 1; index < n; index++){
+            int[] curr = new int[amount+1];
+            for(int target = 0; target<= amount; target++){
+                int notTake = prev[target];
+                int take = 0;
+                if(coins[index] <= target)
+                    take = curr[target-coins[index]];
+
+                curr[target] = take + notTake;
+            }
+            prev = curr;
+        }
+
+        return prev[amount];
+    }
 }
