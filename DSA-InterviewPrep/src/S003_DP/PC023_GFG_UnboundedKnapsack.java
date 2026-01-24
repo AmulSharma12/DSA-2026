98e62c71064c3b22308e5a07e1e43fbe30e3612c
Amul Op <amulsharmarr67@gmail.com>
2026-01-24 17:42:29 +0530

    Dp-23 GFG Unbounded knapsack -memoizatiton solution

--- DSA-InterviewPrep/src/S003_DP/PC023_GFG_UnboundedKnapsack.java
+++ DSA-InterviewPrep/src/S003_DP/PC023_GFG_UnboundedKnapsack.java
@@ -1,4 +1,7 @@
 package S003_DP;
+
+import java.util.Arrays;
+
 //https://www.geeksforgeeks.org/problems/knapsack-with-duplicate-items4201/1
 public class PC023_GFG_UnboundedKnapsack {
     public static void main(String[] args){
@@ -24,5 +27,34 @@ public class PC023_GFG_UnboundedKnapsack {
             included = val[index] + unboundedKnapSackR(index, maxWeight-wt[index], wt, val);
 
         return Math.max(excluded, included);
+    }
+
+
+    //Memoization solution
+    public static int knapSackM(int val[], int wt[], int capacity) {
+        int n = val.length;
+        int[][] dp = new int[n][capacity+1];
+        for(int[] dpRow: dp){
+            Arrays.fill(dpRow,-1);
+        }
+        return unboundedKnapSackM(n-1, capacity, wt, val, dp);
+    }
+
+    private static int unboundedKnapSackM(int index, int maxWeight, int[] wt, int[] val, int[][] dp){
+        //base case
+        if(index == 0){
+            return (maxWeight/wt[0]) * val[0];
+        }
+
+        //if already computed then return
+        if(dp[index][maxWeight] != -1)  return dp[index][maxWeight];
+
+        //fill the knapsack
+        int excluded = unboundedKnapSackM(index-1, maxWeight, wt, val, dp);
+        int included = Integer.MIN_VALUE;
+        if(wt[index] <= maxWeight)
+            included = val[index] + unboundedKnapSackM(index, maxWeight-wt[index], wt, val, dp);
+
+        return dp[index][maxWeight] = Math.max(excluded, included);
     }
 }
