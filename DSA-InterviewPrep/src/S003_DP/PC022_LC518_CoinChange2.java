0052ca2a83b682f22216e0c889e8fd66e6fe5cfd
Amul Op <amulsharmarr67@gmail.com>
2026-01-24 14:21:32 +0530

    DP-22 LC518 Coin change 2 - tabulation

--- DSA-InterviewPrep/src/S003_DP/PC022_LC518_CoinChange2.java
+++ DSA-InterviewPrep/src/S003_DP/PC022_LC518_CoinChange2.java
@@ -54,4 +54,27 @@ public class PC022_LC518_CoinChange2 {
 
         return dp[index][target] = notTake + take;
     }
+
+    //Tabulation solution
+    private static int coinChangeT(int amount, int[] coins){
+        int n = coins.length;
+        int[][] dp = new int[n][amount+1];
+
+        for(int target = 0; target <= amount; target++){
+            if(target%coins[0] == 0) dp[0][target] = 1;
+        }
+
+        for(int index = 1; index < n; index++){
+            for(int target = 0; target<= amount; target++){
+                int notTake = dp[index-1][target];
+                int take = 0;
+                if(coins[index] <= target)
+                    take = dp[index][target-coins[index]];
+
+                dp[index][target] = take + notTake;
+            }
+        }
+
+        return dp[n-1][amount];
+    }
 }
