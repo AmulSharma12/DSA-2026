5efeb8b793ceea7fced2da37bd6af2bdcecf2454
Amul Op <amulsharmarr67@gmail.com>
2026-01-25 15:37:33 +0530

    DP-25 LCS tabulation solution

--- DSA-InterviewPrep/src/S003_DP/PC025_LC1143_LCS.java
+++ DSA-InterviewPrep/src/S003_DP/PC025_LC1143_LCS.java
@@ -54,4 +54,29 @@ public class PC025_LC1143_LCS {
                 Math.max(lcsMemoize(index1-1, index2, text1, text2, cache),
                         lcsMemoize(index1, index2-1, text1, text2, cache));
     }
+
+    //tabulation solution
+    private static int lcsTabulation(String text1, String text2){
+        //base case
+        int n = text1.length();
+        int m = text2.length();
+        int[][] dp = new int[n+1][m+1];
+
+        //shifted to each at right because negative base cases so 0th index treated as -1 base case  thats why for state N index pick would be index-1
+
+        for(int j = 0; j<=m ; j++)   dp[0][j] = 0;
+        for(int i = 0; i<=n; i++)    dp[i][0] = 0;
+
+        //match and not match cases
+        for(int ind1 = 1; ind1 <= n; ind1++){
+            for(int ind2 = 1; ind2 <= m; ind2++){
+                if(text1.charAt(ind1 - 1) == text2.charAt(ind2 - 1))
+                    dp[ind1][ind2] = 1 + dp[ind1-1][ind2-1];
+                else
+                    dp[ind1][ind2] = Math.max(dp[ind1-1][ind2], dp[ind1][ind2-1]);
+            }
+        }
+
+        return dp[n][m];
+    }
 }
