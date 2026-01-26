d7796bea8b485c4644e36a4a02d7657dd33efd5c
Amul Op <amulsharmarr67@gmail.com>
2026-01-26 14:27:37 +0530

    DP-28 LC516 longest palindormic subsequence

--- DSA-InterviewPrep/src/S003_DP/PC028_LC516_LongestPalindromeSubsequence.java
+++ DSA-InterviewPrep/src/S003_DP/PC028_LC516_LongestPalindromeSubsequence.java
@@ -78,4 +78,24 @@ public class PC028_LC516_LongestPalindromeSubsequence {
         return dp[n][m];
     }
 
+    //Space optimization solution
+    private static int lcsSO(String s1, String s2){
+        int n = s1.length();
+        int m = s2.length();
+        int[] prev = new int[m+1];
+
+        for(int ind1 = 1; ind1 <= n; ind1++){
+            int[] curr = new int[m+1];
+            for(int ind2 = 1; ind2 <= m; ind2++){
+                if(s1.charAt(ind1 - 1) == s2.charAt(ind2-1))
+                    curr[ind2] = 1 + prev[ind2-1];
+                else
+                    curr[ind2] = Math.max(curr[ind2-1] , prev[ind2]);
+            }
+            prev = curr;
+        }
+
+        return prev[m];
+    }
+
 }
