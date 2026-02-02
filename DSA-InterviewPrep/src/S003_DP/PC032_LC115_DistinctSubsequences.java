3525474e8d2e84390a0cc5f66815fd3dccd8dea5
Amul Op <amulsharmarr67@gmail.com>
2026-02-02 17:02:41 +0530

    DP-32 LC115 Distinct subsequence - 1D array optimization

--- DSA-InterviewPrep/src/S003_DP/PC032_LC115_DistinctSubsequences.java
+++ DSA-InterviewPrep/src/S003_DP/PC032_LC115_DistinctSubsequences.java
@@ -66,4 +66,47 @@ public class PC032_LC115_DistinctSubsequences {
         }
         return dp[n][m];
     }
+
+    //Space optimization
+    private int numDistinctSO(String s, String t){
+        int n = s.length();
+        int m = t.length();
+
+        int[] prev = new int[m+1];
+        prev[0] = 1;
+
+        for(int i = 1; i<=n; i++){
+            int[] curr = new int[m+1];
+            curr[0] = 1;
+            for(int j = 1; j<=m; j++){
+                if(s.charAt(i-1) == t.charAt(j-1))
+                    curr[j] = prev[j-1] + prev[j];
+                else
+                    curr[j] = prev[j];
+            }
+
+            prev = curr;
+        }
+        return prev[m];
+    }
+
+    //1D array optimization
+    private static int numDistinct1DOptimized(String s, String t){
+        int n = s.length();
+        int m = t.length();
+
+        int[] prev = new int[m+1];
+        prev[0] = 1;
+
+        for(int i = 1; i<=n; i++){
+
+            for(int j = m; j>=1; j--){
+                if(s.charAt(i-1) == t.charAt(j-1))
+                    prev[j] = prev[j-1] + prev[j];
+                else
+                    prev[j] = prev[j];
+            }
+        }
+        return prev[m];
+    }
 }
