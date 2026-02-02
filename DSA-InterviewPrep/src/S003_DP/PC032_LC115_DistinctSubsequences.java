e25c896ba4ab5ac4123a45b1c3bc26bdb608d56f
Amul Op <amulsharmarr67@gmail.com>
2026-02-02 13:37:40 +0530

    DP-32 LC112 distinct subsequences - tabulation solution

--- DSA-InterviewPrep/src/S003_DP/PC032_LC115_DistinctSubsequences.java
+++ DSA-InterviewPrep/src/S003_DP/PC032_LC115_DistinctSubsequences.java
@@ -46,4 +46,24 @@ public class PC032_LC115_DistinctSubsequences {
 
         return dp[ind1][ind2] = numDistinctM(ind1-1, ind2, s, t, dp);
     }
+
+    //Tabulation solution
+    private static int numDistinctT(String s, String t){
+        int n = s.length();
+        int m = t.length();
+
+        int[][] dp = new int[n+1][m+1];
+
+        for(int i = 0; i<=n; i++)    dp[i][0] = 1;
+
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=m; j++){
+                if(s.charAt(i-1) == t.charAt(j-1))
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                else
+                    dp[i][j] = dp[i-1][j];
+            }
+        }
+        return dp[n][m];
+    }
 }
