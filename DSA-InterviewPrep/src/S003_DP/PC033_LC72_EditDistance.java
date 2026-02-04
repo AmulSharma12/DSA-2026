88d5011e892bb44a57a051755ceb5ba8fdbd6b03
Amul Op <amulsharmarr67@gmail.com>
2026-02-04 23:49:40 +0530

    DP-33 LC72 Edit distance - tabulation solution

--- DSA-InterviewPrep/src/S003_DP/PC033_LC72_EditDistance.java
+++ DSA-InterviewPrep/src/S003_DP/PC033_LC72_EditDistance.java
@@ -51,4 +51,26 @@ public class PC033_LC72_EditDistance {
                 Math.min(editDistanceM(i-1, j, s1, s2, cache), editDistanceM(i-1, j-1, s1, s2, cache))
         );
     }
+
+    //tabulation solution
+    private static int editDistanceT(String s1, String s2){
+        int n = s1.length();
+        int m = s2.length();
+        int[][] dp = new int[n+1][m+1];
+        //base case
+        for(int j = 0; j<=m; j++)   dp[0][j] = j;
+        for(int i = 0; i<=n; i++)   dp[i][0] = i;
+
+
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=m; j++){
+                if(s1.charAt(i-1) == s2.charAt(j-1))
+                    dp[i][j] = dp[i-1][j-1];
+                else
+                    dp[i][j] = 1+Math.min(dp[i-1][j-1],Math.min(dp[i][j-1], dp[i-1][j]));
+            }
+        }
+
+        return dp[n][m];
+    }
 }
