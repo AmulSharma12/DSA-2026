b3eb33aafbf95e67f1e4fc619df87200ea028b53
Amul Op <amulsharmarr67@gmail.com>
2026-02-06 00:28:23 +0530

    DP-34 LC44 Wildcard matching - recursive soluttion

---
+++ DSA-InterviewPrep/src/S003_DP/PC034_LC44_WildCardMatching.java
@@ -0,0 +1,34 @@
+package S003_DP;
+
+public class PC034_LC44_WildCardMatching {
+    public static void main(String[] args){
+        //call from here...
+    }
+
+    //Recursive solution
+    public static boolean isMatchREC(String s, String p) {
+        int n = s.length();
+        int m = p.length();
+        return isMatchR(n-1, m-1, s, p);
+    }
+
+    private static boolean isMatchR(int i, int j, String s, String p){
+        //base case
+        if(i<0 && j < 0)    return true;
+        if(j<0 && i>=0)     return false;
+        if(i<0 && j>=0){
+            for(int ind = 0; ind<=j; ind++){
+                if(p.charAt(ind) != '*')    return false;
+            }
+            return true;
+        }
+
+        //if equal or ?
+        if(s.charAt(i) == p.charAt(j) || p.charAt(j) == '?')
+            return isMatchR(i-1, j-1, s, p);
+        if(p.charAt(j) == '*')
+            return isMatchR(i, j-1, s, p) || isMatchR(i-1, j, s, p);
+
+        return false;
+    }
+}
