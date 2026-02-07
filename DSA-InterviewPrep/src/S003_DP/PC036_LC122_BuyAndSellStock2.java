abaa713e84b549ff52275c6790bf0b43d9a37fb6
Amul Op <amulsharmarr67@gmail.com>
2026-02-07 20:46:39 +0530

    DP-35 LC122 Buy and sell stock 2 - space optimization

--- DSA-InterviewPrep/src/S003_DP/PC036_LC122_BuyAndSellStock2.java
+++ DSA-InterviewPrep/src/S003_DP/PC036_LC122_BuyAndSellStock2.java
@@ -94,4 +94,36 @@ public class PC036_LC122_BuyAndSellStock2 {
 
         return dp[0][1];
     }
+
+    //Space optimization
+    private static int maxProfitSO(int[] prices){
+        int n = prices.length;
+
+        int[] ahead = new int[2];
+        ahead[0] = ahead[1] = 0;
+
+        for(int ind = n-1 ; ind >= 0 ; ind--){
+            int[] curr = new int[2];
+            for(int buy = 0; buy <= 1 ; buy++){
+                int profit = 0;
+                if(buy == 1){
+                    profit = Math.max(
+                            -prices[ind] + ahead[0],
+                            ahead[1]
+                    );
+                }else{
+                    profit = Math.max(
+                            prices[ind] + ahead[1],
+                            ahead[0]
+                    );
+                }
+
+                curr[buy] = profit;
+            }
+
+            ahead = curr;
+        }
+
+        return ahead[1];
+    }
 }
