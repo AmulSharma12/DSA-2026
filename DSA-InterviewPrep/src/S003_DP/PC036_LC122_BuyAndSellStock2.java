215510c46c205037f479b93397a6cd1536f230e1
Amul Op <amulsharmarr67@gmail.com>
2026-02-07 19:07:48 +0530

    DP-36 LC122 Buy and sell stock 2 - memoization solution

--- DSA-InterviewPrep/src/S003_DP/PC036_LC122_BuyAndSellStock2.java
+++ DSA-InterviewPrep/src/S003_DP/PC036_LC122_BuyAndSellStock2.java
@@ -1,4 +1,7 @@
 package S003_DP;
+
+import java.util.Arrays;
+
 //https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
 public class PC036_LC122_BuyAndSellStock2 {
     public static void main(String[] args){
@@ -28,5 +31,37 @@ public class PC036_LC122_BuyAndSellStock2 {
         }
 
         return profit;
+    }
+
+
+    //Memoization solution
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int[][] cache = new int[n][2];
+        for(int[] cacheRow:cache){
+            Arrays.fill(cacheRow,-1);
+        }
+        return maxProfitM(0,1, prices, cache);
+    }
+
+    private int maxProfitM(int ind,int buy, int[] prices, int[][] cache){
+        if(ind == prices.length)    return 0;
+
+        if(cache[ind][buy] != -1)   return cache[ind][buy];
+        int profit = Integer.MIN_VALUE;
+        if(buy == 1){
+            profit = Math.max(
+                    -prices[ind] + maxProfitM(ind+1, 0, prices, cache),
+                    0 + maxProfitM(ind+1, 1, prices, cache)
+            );
+
+        }else{
+            profit = Math.max(
+                    prices[ind] + maxProfitM(ind+1, 1, prices, cache),
+                    0 + maxProfitM(ind+1, 0, prices, cache)
+            );
+        }
+
+        return cache[ind][buy] = profit;
     }
 }
