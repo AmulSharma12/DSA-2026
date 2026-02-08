b87f9465762bd52963919050bea23e0b68468a44
Amul Op <amulsharmarr67@gmail.com>
2026-02-08 16:28:57 +0530

    DP-39 LC309 - buy and sell stock with cooldown - tabulation solution

--- DSA-InterviewPrep/src/S003_DP/PC039_LC309_BuyAndSellStockWithCoolDown.java
+++ DSA-InterviewPrep/src/S003_DP/PC039_LC309_BuyAndSellStockWithCoolDown.java
@@ -61,4 +61,28 @@ public class PC039_LC309_BuyAndSellStockWithCoolDown {
                 maxProfitM(day+1, 0, prices, dp)
         );
     }
+
+
+    //Tabulation solution
+    private static int maxProfitT(int[] prices, int n){
+        int[][] dp = new int[n+2][2];
+
+        for(int day = n-1; day>=0; day--){
+            for(int buy = 0; buy<=1; buy++){
+                if(buy == 1){
+                    dp[day][buy] = Math.max(
+                            -prices[day] + dp[day+1][0],
+                            dp[day+1][1]
+                    );
+                }else{
+                    dp[day][buy] = Math.max(
+                            prices[day] + dp[day+2][1],
+                            dp[day+1][0]
+                    );
+                }
+            }
+        }
+
+        return dp[0][1];
+    }
 }
