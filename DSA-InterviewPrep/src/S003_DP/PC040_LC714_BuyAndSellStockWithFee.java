de041933bdf423b0325dc6478837e467894008af
Amul Op <amulsharmarr67@gmail.com>
2026-02-08 17:18:10 +0530

    DP-40 LC714 buy and sell stock with transaction fee - memoization solution

--- DSA-InterviewPrep/src/S003_DP/PC040_LC714_BuyAndSellStockWithFee.java
+++ DSA-InterviewPrep/src/S003_DP/PC040_LC714_BuyAndSellStockWithFee.java
@@ -1,4 +1,5 @@
 package S003_DP;
+import java.util.Arrays;
 //https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/description/
 public class PC040_LC714_BuyAndSellStockWithFee {
     public static void main(String[] args){
@@ -24,6 +25,36 @@ public class PC040_LC714_BuyAndSellStockWithFee {
         return Math.max(
                 prices[day] - fee + maxProfitR(day+1, 1, prices, fee),
                 maxProfitR(day+1, 0, prices, fee)
+        );
+    }
+
+
+    //Memoization  solution
+    public static int maxProfitMem(int[] prices, int fee) {
+        int n = prices.length;
+        int[][] dp = new int[n][2];
+        for(int[] dpRow: dp){
+            Arrays.fill(dpRow,-1);
+        }
+        return maxProfitM(0, 1, prices, fee, dp);
+    }
+
+    private static int maxProfitM(int day, int buy, int[] prices, int fee, int[][] dp){
+        if(day == prices.length)    return 0;
+
+        if(dp[day][buy] != -1)  return dp[day][buy];
+
+        if(buy == 1){
+            return dp[day][buy] = Math.max(
+                    -prices[day] + maxProfitM(day+1, 0, prices, fee, dp),
+                    maxProfitM(day+1, 1, prices, fee, dp)
+            );
+        }
+
+
+        return dp[day][buy] = Math.max(
+                prices[day] - fee + maxProfitM(day+1, 1, prices, fee, dp),
+                maxProfitM(day+1, 0, prices, fee, dp)
         );
     }
 }
