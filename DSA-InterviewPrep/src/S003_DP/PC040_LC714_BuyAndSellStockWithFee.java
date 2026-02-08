44cab8354b6d14c0e31d1f9b64f2753311040c00
Amul Op <amulsharmarr67@gmail.com>
2026-02-08 17:30:17 +0530

    DP-40 LC714 buy and sell stock with fee - tabulation solution

--- DSA-InterviewPrep/src/S003_DP/PC040_LC714_BuyAndSellStockWithFee.java
+++ DSA-InterviewPrep/src/S003_DP/PC040_LC714_BuyAndSellStockWithFee.java
@@ -57,4 +57,29 @@ public class PC040_LC714_BuyAndSellStockWithFee {
                 maxProfitM(day+1, 0, prices, fee, dp)
         );
     }
+
+
+    //Tabulation solution
+    private static int maxProfitT(int[] prices, int n, int fee){
+        int[][] dp = new int[n+1][2];
+
+        for(int day = n-1; day>=0; day--){
+            for(int buy = 0; buy<=1 ; buy++){
+                if(buy == 1){
+                    dp[day][buy] = Math.max(
+                            -prices[day] + dp[day+1][0],
+                            dp[day+1][1]
+                    );
+                }else{
+                    dp[day][buy] = Math.max(
+                            prices[day] - fee + dp[day+1][1],
+                            dp[day+1][0]
+                    );
+                }
+            }
+        }
+
+
+        return dp[0][1];
+    }
 }
