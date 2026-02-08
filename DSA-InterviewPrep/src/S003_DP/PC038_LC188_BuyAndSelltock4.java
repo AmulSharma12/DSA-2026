8cb90ca7d02401d1e592651020107e41124d1cd5
Amul Op <amulsharmarr67@gmail.com>
2026-02-08 13:18:45 +0530

    DP-38 LC188 buy and sell stock 4 - memoization solution

--- DSA-InterviewPrep/src/S003_DP/PC038_LC188_BuyAndSelltock4.java
+++ DSA-InterviewPrep/src/S003_DP/PC038_LC188_BuyAndSelltock4.java
@@ -1,4 +1,7 @@
 package S003_DP;
+
+import java.util.Arrays;
+
 //https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iv/
 public class PC038_LC188_BuyAndSelltock4 {
     public static void main(String[] args){
@@ -27,6 +30,39 @@ public class PC038_LC188_BuyAndSelltock4 {
         return Math.max(
                 prices[day] + maxProfitR(day+1, 1, transactionLimit-1, prices),
                 maxProfitR(day+1, 0, transactionLimit, prices)
+        );
+    }
+
+    public static int maxProfitMem(int k, int[] prices) {
+        int n = prices.length;
+        int[][][] dp = new int[n][2][k+1];
+        for(int day = 0; day < n; day++){
+            for(int buy =0; buy<=1; buy++){
+                Arrays.fill(dp[day][buy] ,-1);
+            }
+        }
+        return maxProfitM(0, 1, k, prices, dp);
+    }
+
+    //Memoization solution
+    private static int maxProfitM(int day, int buy, int transactionLimit, int[] prices, int[][][] dp){
+        //base case
+        if(day == prices.length || transactionLimit == 0) return 0;
+
+        if(dp[day][buy][transactionLimit] != -1)    return dp[day][buy][transactionLimit];
+
+        //recursive calls
+        int profit = 0;
+        if(buy == 1){
+            return dp[day][buy][transactionLimit] = Math.max(
+                    -prices[day] + maxProfitM(day+1, 0, transactionLimit, prices, dp),
+                    maxProfitM(day+1, 1, transactionLimit, prices, dp)
+            );
+        }
+
+        return dp[day][buy][transactionLimit] = Math.max(
+                prices[day] + maxProfitM(day+1, 1, transactionLimit-1, prices, dp),
+                maxProfitM(day+1, 0, transactionLimit, prices, dp)
         );
     }
 }
