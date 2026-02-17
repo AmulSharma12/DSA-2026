9f7168965b357a5abb9e618052506f38115473fe
Amul Op <amulsharmarr67@gmail.com>
2026-02-18 00:48:52 +0530

    DP-48 GFG MCM matrix chain multiplication - memoization solution

--- DSA-InterviewPrep/src/S003_DP/PC048_GFG_MCM.java
+++ DSA-InterviewPrep/src/S003_DP/PC048_GFG_MCM.java
@@ -1,4 +1,5 @@
 package S003_DP;
+import java.util.Arrays;
 //https://www.geeksforgeeks.org/problems/matrix-chain-multiplication0303/1
 public class PC048_GFG_MCM {
     public static void main(String[] args){
@@ -22,5 +23,33 @@ public class PC048_GFG_MCM {
         }
 
         return mini;
+    }
+
+
+    //Memoization solution
+    static int matrixMultiplication(int arr[]) {
+        int n = arr.length;
+        int[][] dp = new int[n][n];
+        for(int[] dpRow:dp){
+            Arrays.fill(dpRow,-1);
+        }
+        return mcmMemoize(1, n-1, arr,dp);
+    }
+
+    private static int mcmMemoize(int i, int j, int[] arr, int[][] dp){
+        if(i == j)  return 0;
+
+        if(dp[i][j] != -1)  return dp[i][j];
+
+        int mini = Integer.MAX_VALUE;
+        for(int k = i; k<j; k++){
+            int steps = arr[i-1] * arr[k] * arr[j]
+                    + mcmMemoize(i, k, arr,dp)
+                    + mcmMemoize(k+1, j, arr, dp);
+
+            mini = Math.min(mini, steps);
+        }
+
+        return dp[i][j] = mini;
     }
 }
