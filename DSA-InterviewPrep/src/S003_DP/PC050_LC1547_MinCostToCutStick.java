49a50a847c7e41373cf7ad78f7501a117ef715df
Amul Op <amulsharmarr67@gmail.com>
2026-02-22 00:26:26 +0530

    DP-50 LC1547 Min cost to cut stick - memoization solution

--- DSA-InterviewPrep/src/S003_DP/PC050_LC1547_MinCostToCutStick.java
+++ DSA-InterviewPrep/src/S003_DP/PC050_LC1547_MinCostToCutStick.java
@@ -32,4 +32,34 @@ public class PC050_LC1547_MinCostToCutStick {
 
         return mini;
     }
+
+
+    //Memoization solution
+    public static int minCostMem(int n, int[] cuts) {
+        int m = cuts.length;
+        int[] arr = new int[m + 2];
+        arr[0] = 0;
+        arr[m + 1] = n;
+        for (int i = 0; i < m; i++) arr[i + 1] = cuts[i];
+        Arrays.sort(arr);
+        int[][] dp = new int[m+1][m+1];
+        for(int[] dpRow: dp){
+            Arrays.fill(dpRow,-1);
+        }
+        return minCostM(1, m,  arr, n, dp);
+    }
+
+
+    private static int minCostM(int i, int j, int[] cuts, int n, int[][] dp){
+        if(i > j)   return 0;
+        if(dp[i][j] != -1)  return dp[i][j];
+
+        int mini = Integer.MAX_VALUE;
+        for(int k = i; k<=j; k++){
+            int cost = cuts[j+1] - cuts[i-1] + minCostM(i, k-1, cuts, n, dp) + minCostM(k+1, j,  cuts, n, dp);
+            mini = Math.min(mini, cost);
+        }
+
+        return dp[i][j] = mini;
+    }
 }
