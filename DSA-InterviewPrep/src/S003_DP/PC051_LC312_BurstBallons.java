5a769511bd3b21dfca67eb8eea590aeb1f37024b
Amul Op <amulsharmarr67@gmail.com>
2026-02-22 17:25:18 +0530

    DP-51 LC312 burst ballons - memoization solution

--- DSA-InterviewPrep/src/S003_DP/PC051_LC312_BurstBallons.java
+++ DSA-InterviewPrep/src/S003_DP/PC051_LC312_BurstBallons.java
@@ -1,5 +1,5 @@
 package S003_DP;
-
+import java.util.Arrays;
 public class PC051_LC312_BurstBallons {
     public static void main(String[] args){
         //call from here...
@@ -28,5 +28,34 @@ public class PC051_LC312_BurstBallons {
         }
 
         return maxi;
+    }
+
+
+    //Memoization solution
+    public static int maxCoinsMem(int[] ballons) {
+        int n = ballons.length;
+        int[] nums = new int[n+2];
+        nums[0] = 1;
+        nums[n+1] = 1;
+        for(int ind = 0; ind < n; ind++)    nums[ind+1] = ballons[ind];
+        int[][] dp = new int[n+1][n+1];
+        for(int[] dpRow: dp)
+            Arrays.fill(dpRow,-1);
+        return maxCoinsMem(1, n, nums, dp);
+    }
+
+    private static int maxCoinsMem(int i, int j, int[] nums, int[][] dp){
+        if(i > j)   return 0;
+        if(dp[i][j] != -1)  return dp[i][j];
+        int maxi = 0;
+        for(int burst = i; burst<=j; burst++){
+            int coins = nums[burst] * nums[i-1] * nums[j+1]
+                    + maxCoinsMem(i, burst-1, nums, dp)
+                    + maxCoinsMem(burst+1, j, nums, dp);
+
+            maxi = Math.max(maxi, coins);
+        }
+
+        return dp[i][j] =  maxi;
     }
 }
