f339992ba8948b582b0fad0ba53987c1a2a9735c
Amul Op <amulsharmarr67@gmail.com>
2026-02-28 15:39:00 +0530

    DP-54 LC1043 Partition array max sum - memoization solution

--- DSA-InterviewPrep/src/S003_DP/PC054_LC1043_PartitionArrayMaxSum.java
+++ DSA-InterviewPrep/src/S003_DP/PC054_LC1043_PartitionArrayMaxSum.java
@@ -1,4 +1,7 @@
 package S003_DP;
+
+import java.util.Arrays;
+
 //https://leetcode.com/problems/partition-array-for-maximum-sum/
 public class PC054_LC1043_PartitionArrayMaxSum {
     public static void main(String[] args){
@@ -25,5 +28,32 @@ public class PC054_LC1043_PartitionArrayMaxSum {
         }
 
         return maxSum;
+    }
+
+
+    //Memoization solution
+    public static int maxSumAfterPartitioningM(int[] arr, int k) {
+        int n = arr.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, -1);
+        return maxSumMemoize(0, n, arr, k, dp);
+    }
+
+    private  static int maxSumMemoize(int ind, int n, int[] nums, int k, int[] dp){
+        if(ind == n)    return 0;
+
+        if(dp[ind] != -1)   return dp[ind];
+
+        int length = 0;
+        int maximum = Integer.MIN_VALUE;
+        int maxSum = Integer.MIN_VALUE;
+        for(int j = ind; j<Math.min(n, ind+k); j++){
+            length++;
+            maximum = Math.max(maximum, nums[j]);
+            int summation = (maximum * length) + maxSumMemoize(j+1, n, nums, k, dp);
+            maxSum = Math.max(maxSum, summation);
+        }
+
+        return dp[ind] = maxSum;
     }
 }
