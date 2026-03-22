60c831dc5e71e69830a21eb6ef16990b10e3c763
Amul Op <amulsharmarr67@gmail.com>
2026-03-22 20:23:39 +0530

    TP-SW-3 LC1004 max consecutive Ones III - optimised 1

--- DSA-InterviewPrep/src/S004_TwoPointerSlidingWindow/P003_LC1004_MaxConsecutiveOnesIII.java
+++ DSA-InterviewPrep/src/S004_TwoPointerSlidingWindow/P003_LC1004_MaxConsecutiveOnesIII.java
@@ -25,4 +25,29 @@ public class P003_LC1004_MaxConsecutiveOnesIII {
 
         return maxLength;
     }
+
+
+    //Approach 2 - optimised handling zero in window using while loop
+    private static int longestOnesOptimised1(int[] nums, int k) {
+        int n = nums.length;
+        int l = 0;
+        int r = 0;
+        int maxLength = 0;
+        int zero = 0;
+        while(r < n){
+            if(nums[r] == 0)    zero++;
+
+            while(zero > k){
+                if(nums[l] == 0) zero--;
+                l++;
+            }
+
+            if(zero <= k)
+                maxLength = Math.max(maxLength, r-l+1);
+
+            r++;
+        }
+
+        return maxLength;
+    }
 }
