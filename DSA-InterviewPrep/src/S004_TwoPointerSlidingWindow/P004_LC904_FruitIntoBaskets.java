44f11e6826698af189f09c44f90219630167bdbc
Amul Op <amulsharmarr67@gmail.com>
2026-03-26 01:03:29 +0530

    TP-SW-3 LC904 fruitt into baskets - using sliding window

--- DSA-InterviewPrep/src/S004_TwoPointerSlidingWindow/P004_LC904_FruitIntoBaskets.java
+++ DSA-InterviewPrep/src/S004_TwoPointerSlidingWindow/P004_LC904_FruitIntoBaskets.java
@@ -1,6 +1,5 @@
 package S004_TwoPointerSlidingWindow;
-import java.util.Set;
-import java.util.HashSet;
+import java.util.*;
 //https://leetcode.com/problems/fruit-into-baskets/description/
 public class P004_LC904_FruitIntoBaskets {
     public static void main(String[] args){
@@ -19,6 +18,41 @@ public class P004_LC904_FruitIntoBaskets {
                 maxLength = Math.max(maxLength, r-l+1);
             }
         }
+        return maxLength;
+    }
+
+
+    //Approach 2 - using sliding window
+    private static int totalFruitUsingSlidingWindow(int[] fruits) {
+        int l = 0;
+        int r = 0;
+        int maxLength = 0;
+        int n = fruits.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        //logic
+        while(r < n){
+            int key = fruits[r];
+            map.put(key, map.getOrDefault(key, 0) + 1);
+            if(map.size() > 2){
+                while(map.size() > 2){
+                    int leftKey = fruits[l];
+                    map.put(leftKey, map.get(leftKey) - 1);
+                    if(map.get(leftKey) == 0){
+                        map.remove(leftKey);
+                    }
+
+                    l++;
+                }
+
+            }
+
+            if(map.size() <= 2)
+                maxLength = Math.max(maxLength , r-l+1);
+
+            r++;
+        }
+
         return maxLength;
     }
 }
