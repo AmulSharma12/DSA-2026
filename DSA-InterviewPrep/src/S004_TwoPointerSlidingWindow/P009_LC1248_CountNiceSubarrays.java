c987d1a42617cc285064c7f9736eb024e113fa32
Amul Op <amulsharmarr67@gmail.com>
2026-04-04 19:09:13 +0530

    TP-SW-9 Count nice subarrasy - optimized based on TP-SW and math logic based on previous binary problem

--- DSA-InterviewPrep/src/S004_TwoPointerSlidingWindow/P009_LC1248_CountNiceSubarrays.java
+++ DSA-InterviewPrep/src/S004_TwoPointerSlidingWindow/P009_LC1248_CountNiceSubarrays.java
@@ -23,4 +23,34 @@ public class P009_LC1248_CountNiceSubarrays {
 
         return cnt;
     }
+
+
+    //Approach 2 - using TP-SW and by using maths logic based on previous binary problem
+    public static int numberOfSubarraysOptimized(int[] nums, int k) {
+        return helper(nums, k) - helper(nums, k-1);
+    }
+
+    private static int helper(int[] nums, int k){
+        if(k < 0)   return 0;
+
+        int l = 0;
+        int r = 0;
+        int cnt = 0;
+        int sum = 0;
+        int n = nums.length;
+
+        while(r < n){
+            sum += nums[r]%2 == 0 ? 0 : 1;
+
+            while(sum > k){
+                sum -= nums[l]%2 == 0 ? 0 : 1;
+                l++;
+            }
+
+            cnt += (r-l+1);
+            r++;
+        }
+
+        return cnt;
+    }
 }
