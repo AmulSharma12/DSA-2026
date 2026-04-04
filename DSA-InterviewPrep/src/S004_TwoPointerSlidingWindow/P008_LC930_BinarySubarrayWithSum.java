fce4ac1216d817a029f1c6e74e8974dd2e0f7189
Amul Op <amulsharmarr67@gmail.com>
2026-04-04 17:28:08 +0530

    TP-SW-8 LC930 binary subarray with sum - Optimised solution using TP-SW and a maths logic

--- DSA-InterviewPrep/src/S004_TwoPointerSlidingWindow/P008_LC930_BinarySubarrayWithSum.java
+++ DSA-InterviewPrep/src/S004_TwoPointerSlidingWindow/P008_LC930_BinarySubarrayWithSum.java
@@ -20,4 +20,33 @@ public class P008_LC930_BinarySubarrayWithSum {
 
         return cnt;
     }
+
+
+    //Approach  2 - using TP-SW using some maths logic
+    public static int numSubarraysWithSumOptimized(int[] nums, int goal) {
+        return helper(nums, goal) - helper(nums, goal-1);
+    }
+
+    private static int helper(int[] nums, int goal){
+        if(goal < 0)    return 0;
+
+        int l = 0;
+        int r = 0;
+        int cnt = 0;
+        int sum = 0;
+        int n = nums.length;
+        while(r < n){
+            sum += nums[r];
+
+            while(sum > goal){
+                sum -= nums[l];
+                l++;
+            }
+
+            cnt += (r-l+1);
+            r++;
+        }
+
+        return cnt;
+    }
 }
