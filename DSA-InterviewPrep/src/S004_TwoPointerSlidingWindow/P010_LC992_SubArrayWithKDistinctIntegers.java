bfcf416b8841beecb3e0741475dab9d5073d8abf
Amul Op <amulsharmarr67@gmail.com>
2026-04-05 17:24:28 +0530

    TP-SW-10 LC992 subarray with k distinct integer - optimised using TP-SW using maths logic

--- DSA-InterviewPrep/src/S004_TwoPointerSlidingWindow/P010_LC992_SubArrayWithKDistinctIntegers.java
+++ DSA-InterviewPrep/src/S004_TwoPointerSlidingWindow/P010_LC992_SubArrayWithKDistinctIntegers.java
@@ -1,6 +1,7 @@
 package S004_TwoPointerSlidingWindow;
 import java.util.Set;
 import java.util.HashSet;
+import java.util.HashMap;
 //https://leetcode.com/problems/subarrays-with-k-different-integers/
 public class P010_LC992_SubArrayWithKDistinctIntegers {
     public static void main(String[] args){
@@ -18,6 +19,36 @@ public class P010_LC992_SubArrayWithKDistinctIntegers {
                 if(set.size() > k)  break;
                 if(set.size() == k) cnt++;
             }
+        }
+
+        return cnt;
+    }
+
+
+    //Approach 2 - using TP-SW (using maths logic)
+    private static  int subarraysWithKDistinctOptimized(int[] nums, int k) {
+        return helper(nums, k) - helper(nums, k-1);
+    }
+
+    private static int helper(int[] nums, int k){
+        int n = nums.length;
+        int l = 0;
+        int r = 0;
+        int cnt = 0;
+        if(k < 0)  return 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        while(r < n){
+            map.put(nums[r], map.getOrDefault(nums[r], 0)+1);
+            while(map.size() > k){
+                map.put(nums[l], map.getOrDefault(nums[l],0)-1);
+                if(map.get(nums[l]) == 0)
+                    map.remove(nums[l]);
+                l++;
+            }
+
+            if(map.size() <= k)
+                cnt += (r-l+1);
+            r++;
         }
 
         return cnt;
