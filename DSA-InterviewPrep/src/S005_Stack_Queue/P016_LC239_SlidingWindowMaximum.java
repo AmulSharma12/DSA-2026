c413cdf0e520b42cc6d0992516953ca2cff4d90a
Amul Op <amulsharmarr67@gmail.com>
2026-04-28 01:53:54 +0530

    P016 - LC239 Sliding window maximum - deque optimized approach

--- DSA-InterviewPrep/src/S005_Stack_Queue/P016_LC239_SlidingWindowMaximum.java
+++ DSA-InterviewPrep/src/S005_Stack_Queue/P016_LC239_SlidingWindowMaximum.java
@@ -1,4 +1,6 @@
 package S005_Stack_Queue;
+import java.util.Deque;
+import java.util.ArrayDeque;
 //https://leetcode.com/problems/sliding-window-maximum/description/
 public class P016_LC239_SlidingWindowMaximum {
     public static void main(String[] args){
@@ -18,6 +20,31 @@ public class P016_LC239_SlidingWindowMaximum {
 
             result[ind] = maxi;
         }
+
+        return result;
+    }
+
+
+    //Approach 2 - using montonic stack decreasing order
+    //so that from the front i will be having the largest element and followed by the lesser element from them
+    //remove unneccessary elements if required
+    public static int[] maxSlidingWindowOptimized(int[] nums, int k) {
+        int n = nums.length;
+        int[] result = new int[n-k+1];
+        int i = 0;
+        Deque<Integer> q = new ArrayDeque<>();
+
+        for(int ind = 0; ind < n; ind++){
+            //removing out of boundary from the front
+            if(!q.isEmpty() && q.peek() == ind-k)  q.poll();
+
+            //removing all lesser elements
+            while(!q.isEmpty() && nums[q.peekLast()] < nums[ind]) q.pollLast();
+
+            q.offer(ind);
+            if(ind >= k-1)    result[i++] = nums[q.peek()];
+        }
+
 
         return result;
     }
