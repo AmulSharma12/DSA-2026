7625f0881bc14322559f6ebfb9ebfcb116169218
Amul Op <amulsharmarr67@gmail.com>
2026-05-06 00:00:17 +0530

    BS P005 LC81 Search rotated sorted array 2 - using binary search approach

--- DSA-InterviewPrep/src/S006_BinarySearch/P005_LC81_SearchRotatedSortedArray2.java
+++ DSA-InterviewPrep/src/S006_BinarySearch/P005_LC81_SearchRotatedSortedArray2.java
@@ -14,4 +14,41 @@ public class P005_LC81_SearchRotatedSortedArray2 {
 
         return false;
     }
+
+    //Approach 2 - using binary search algorithim
+    public static boolean searchBinarySearch(int[] nums, int target) {
+        int n = nums.length;
+        int low = 0;
+        int high = n-1;
+
+        while(low <= high){
+            int mid = (low + high)/2;
+            if(nums[mid] == target) return true;
+
+            //if all the element at ind - low/mid/high are equal shrink the window
+            if(nums[low] == nums[mid] && nums[mid] == nums[high])   {
+                low++;
+                high--;
+                continue;
+            }
+
+
+            if(nums[low] <= nums[mid]){
+                //left part sorted
+                if(nums[low] <= target && target <= nums[mid])
+                    high = mid - 1;
+                else
+                    low = mid + 1;
+            }else{
+                //right part sorted
+                if(nums[mid] <= target && target <= nums[high])
+                    low = mid + 1;
+                else
+                    high = mid -1;
+            }
+        }
+
+
+        return false;
+    }
 }
