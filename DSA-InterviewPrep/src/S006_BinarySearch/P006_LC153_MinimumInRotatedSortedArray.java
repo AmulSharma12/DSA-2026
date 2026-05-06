39d9897fdd113df4b25450755f6cc560544f2317
Amul Op <amulsharmarr67@gmail.com>
2026-05-06 12:31:56 +0530

    BS P006 LC153 Minimum in sorted rotated array - binary search approach

--- DSA-InterviewPrep/src/S006_BinarySearch/P006_LC153_MinimumInRotatedSortedArray.java
+++ DSA-InterviewPrep/src/S006_BinarySearch/P006_LC153_MinimumInRotatedSortedArray.java
@@ -16,4 +16,28 @@ public class P006_LC153_MinimumInRotatedSortedArray {
 
         return mini;
     }
+    //Approach 2 - using binary search approach
+    public static int findMin(int[] nums) {
+        int n = nums.length;
+        int low = 0;
+        int high = n-1;
+        int mini = Integer.MAX_VALUE;
+
+        if(nums[low] < nums[high])  return nums[low];
+
+        while(low <= high){
+            int mid = (low + high)/2;
+            if(nums[low] <= nums[mid]){
+                mini = Math.min(mini, nums[low]);
+                low = mid + 1;
+            }
+            else {
+                mini = Math.min(mini, nums[mid]);
+                high = mid - 1;
+            }
+
+        }
+
+        return mini;
+    }
 }
