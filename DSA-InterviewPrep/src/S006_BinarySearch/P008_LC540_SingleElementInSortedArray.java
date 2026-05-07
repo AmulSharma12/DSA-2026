6029affc4f46089b99147bc110bac88f8eaae36b
Amul Op <amulsharmarr67@gmail.com>
2026-05-07 15:27:48 +0530

    BS P008 LC540 Single element in sorted array - binary search approach

--- DSA-InterviewPrep/src/S006_BinarySearch/P008_LC540_SingleElementInSortedArray.java
+++ DSA-InterviewPrep/src/S006_BinarySearch/P008_LC540_SingleElementInSortedArray.java
@@ -28,4 +28,39 @@ public class P008_LC540_SingleElementInSortedArray {
 
         return xor;
     }
+
+    //Approach 3 - using binary search approach
+    public static int singleNonDuplicateUsingBinarySearch(int[] nums) {
+        int n = nums.length;
+        if(n == 1)  return nums[0];
+
+        if(nums[0] != nums[1])  return nums[0];
+        if(nums[n-1] != nums[n-2])  return nums[n-1];
+
+        int low = 1;
+        int high = n-2;
+
+        while(low <= high){
+            int mid = (low + high)/2;
+            if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1])    return nums[mid];
+
+
+            if(mid%2 == 0){
+                //mid - even
+                if(nums[mid] == nums[mid+1])
+                    low = mid + 1;
+                else
+                    high = mid - 1;
+            }else{
+                //mid - odd
+                if(nums[mid] == nums[mid-1])
+                    low = mid + 1;
+                else
+                    high = mid - 1;
+            }
+        }
+
+        return -1;
+
+    }
 }
