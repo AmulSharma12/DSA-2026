fb971b126fb9e63621ca34325c9521fbf9be637d
Amul Op <amulsharmarr67@gmail.com>
2026-05-14 00:10:07 +0530

    BS P011 GFG Nth root of number - binary search approach

--- DSA-InterviewPrep/src/S006_BinarySearch/P011_GFG_NthRootOfNumber.java
+++ DSA-InterviewPrep/src/S006_BinarySearch/P011_GFG_NthRootOfNumber.java
@@ -30,4 +30,25 @@ public class P011_GFG_NthRootOfNumber {
 
         return finalProduct;
     }
+
+
+    //binary search approach
+    public static int nthRootUsingBinaryApproach(int n, int m) {
+        int num = 1;
+        if(m == 0)  return 0;
+        int low = 1;
+        int high = m;
+
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            int ans = power(mid, n);
+            if(ans == m)    return mid;
+            else if(ans > m)    high = mid -1;
+            else low = mid + 1;
+        }
+
+        return -1;
+    }
+
+
 }
