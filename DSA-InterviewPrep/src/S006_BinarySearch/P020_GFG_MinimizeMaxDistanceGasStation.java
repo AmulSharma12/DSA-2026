c0ecc3729c5b2372c2dbfe42e297136e9cb95097
Amul Op <amulsharmarr67@gmail.com>
2026-05-23 16:30:37 +0530

    P020 GFG Minimize max distance gas statsion - priority queue approach

--- DSA-InterviewPrep/src/S006_BinarySearch/P020_GFG_MinimizeMaxDistanceGasStation.java
+++ DSA-InterviewPrep/src/S006_BinarySearch/P020_GFG_MinimizeMaxDistanceGasStation.java
@@ -1,5 +1,6 @@
 package S006_BinarySearch;
-
+import java.util.PriorityQueue;
+//https://www.geeksforgeeks.org/problems/minimize-max-distance-to-gas-station/1
 public class P020_GFG_MinimizeMaxDistanceGasStation {
     public static void main(String[] args){
         //call from here...
@@ -43,5 +44,44 @@ public class P020_GFG_MinimizeMaxDistanceGasStation {
         return maxAns;
 
 
+    }
+
+
+
+     static class  Pair{
+        double value;
+        int ind;
+
+        public Pair(double value, int ind){
+            this.value = value;
+            this.ind = ind;
+        }
+    }
+
+
+    //Approach 2-using priority queue
+    public static double minMaxDistUsinPQ(int[] stations, int k) {
+        int n = stations.length;
+        if(n == 1)  return 0.0;
+        int[] howMany = new int[n-1];
+
+        PriorityQueue<Pair> pq = new PriorityQueue<>(
+                (a,b) -> Double.compare(b.value, a.value)
+        );
+
+        for(int ind = 0; ind < n-1; ind++){
+            pq.offer(new Pair(stations[ind+1]-stations[ind], ind));
+        }
+
+        for(int ind = 1; ind <= k; ind++){
+            Pair maxPair = pq.poll();
+            int sectionInd = maxPair.ind;
+            howMany[sectionInd]++;
+            double diff = stations[sectionInd+1] - stations[sectionInd];
+            double sectionLength = diff/(double)(howMany[sectionInd]+1);
+            pq.offer(new Pair(sectionLength, sectionInd));
+        }
+
+        return pq.peek().value;
     }
 }
