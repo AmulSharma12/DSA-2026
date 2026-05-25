9c22ae2a5883d3425404479d52a7b28d07fb0f7c
Amul Op <amulsharmarr67@gmail.com>
2026-05-26 01:56:19 +0530

    BS P021 LC4 median of two sorted arrays - using binary search approach

--- DSA-InterviewPrep/src/S006_BinarySearch/P021_LC4_MediaOfTwoSortedArray.java
+++ DSA-InterviewPrep/src/S006_BinarySearch/P021_LC4_MediaOfTwoSortedArray.java
@@ -24,7 +24,7 @@ public class P021_LC4_MediaOfTwoSortedArray {
         }
 
 
-        Arrays.sort(nums);  //sorting to align the elemnet in sorted array
+        Arrays.sort(nums);  //sorting to align the element in sorted array
 
         if(x == 1)  return (double)nums[0];
         if(x%2 != 0)    return (double) nums[x/2];
@@ -111,5 +111,43 @@ public class P021_LC4_MediaOfTwoSortedArray {
 
         return (double) (mid + prevMid)/2;
 
+    }
+
+
+    //Approach 4 - Using binary search approach
+    public static double findMedianSortedArraysUsingBinarySearchApproach(int[] nums1, int[] nums2) {
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        if(n2 < n1) return findMedianSortedArraysUsingBinarySearchApproach(nums2, nums1);
+        int n = n1 + n2;
+        int leftPart = (n+1)/2;
+        int low = 0;
+        int high = n1;
+
+        while(low <= high){
+            int mid1 = low + (high - low)/2;
+            int mid2 = leftPart - mid1;
+
+            int l1 = Integer.MIN_VALUE;
+            int l2 = Integer.MIN_VALUE;
+            int r1 = Integer.MAX_VALUE;
+            int r2 = Integer.MAX_VALUE;
+
+            if(mid1 < n1)   r1 = nums1[mid1];
+            if(mid2 < n2)   r2 = nums2[mid2];
+            if(mid1-1 >= 0) l1 = nums1[mid1-1];
+            if(mid2-1 >= 0) l2 = nums2[mid2-1];
+
+            if(l1 <= r2 &&  l2 <= r1){
+                if(n%2 != 0)    return (double)Math.max(l1, l2);
+                return (double) (Math.max(l1, l2) + Math.min(r1, r2))/2;
+            }else if(l1 > r2){
+                high = mid1 - 1;
+            }else{
+                low = mid1 + 1;
+            }
+        }
+
+        return 0;
     }
 }
