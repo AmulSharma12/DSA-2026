1b95bf0d9e2e595b32fcc9680c6a51ab98357255
Amul Op <amulsharmarr67@gmail.com>
2026-05-28 01:18:06 +0530

    BS P023 LC2643 - Matrix with max ones - binary search approach

--- DSA-InterviewPrep/src/S006_BinarySearch/P023_LC2643_MatrixMaxOnes.java
+++ DSA-InterviewPrep/src/S006_BinarySearch/P023_LC2643_MatrixMaxOnes.java
@@ -1,4 +1,7 @@
 package S006_BinarySearch;
+
+import java.util.Arrays;
+
 //https://leetcode.com/problems/row-with-maximum-ones/description/
 public class P023_LC2643_MatrixMaxOnes {
     public static void main(String[] args){
@@ -18,6 +21,39 @@ public class P023_LC2643_MatrixMaxOnes {
                 if(mat[row][col] == 1)  numberOfOne++;
             }
 
+            if(numberOfOne > maxOnes){
+                maxOnes = numberOfOne;
+                maxInd = row;
+            }
+        }
+
+        return new int[]{maxInd, maxOnes};
+    }
+
+
+    private static int findNumberOfOnes(int[] nums, int target){
+        Arrays.sort(nums);  //sorting to apply binary search
+        int low = 0;
+        int high = nums.length - 1;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(nums[mid] == target)
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+
+        return nums.length - low;  //number of elements.
+    }
+    public static int[] rowAndMaximumOnesUsingBinarySearch(int[][] mat) {
+        int n = mat.length;
+        int m = mat[0].length;
+        int maxInd = 0;
+        int maxOnes = Integer.MIN_VALUE;
+
+        for(int row = 0; row < n; row++){
+            int numberOfOne = findNumberOfOnes(mat[row], 1);
+            System.out.println("row " + row  + " " + numberOfOne);
             if(numberOfOne > maxOnes){
                 maxOnes = numberOfOne;
                 maxInd = row;
