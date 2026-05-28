8f77177a367be6de91c2681038d09e35c0c85bbb
Amul Op <amulsharmarr67@gmail.com>
2026-05-29 01:14:52 +0530

    P024 LC74 Search 2d matrix - binary search approach

--- DSA-InterviewPrep/src/S006_BinarySearch/P024_LC74_Search2DMatrix.java
+++ DSA-InterviewPrep/src/S006_BinarySearch/P024_LC74_Search2DMatrix.java
@@ -22,4 +22,37 @@ public class P024_LC74_Search2DMatrix {
 
         return isTargetFound;
     }
+
+
+    //Approach 2 - using binary search approach
+    private static boolean binarySearch(int[] nums, int target , int low , int high){
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(nums[mid] == target) return true;
+            else if(target > nums[mid]) low = mid + 1;
+            else high = mid - 1;
+        }
+
+        return false;
+    }
+
+    public static boolean searchMatrixUsingBinarySearch(int[][] matrix, int target) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int startRow = 0;
+        int endRow = n-1;
+
+        while(startRow <= endRow){
+            int midRow = startRow + (endRow - startRow)/2;
+            if(target >= matrix[midRow][0] && target <= matrix[midRow][m-1])
+                return binarySearch(matrix[midRow] , target, 0, m-1);
+            else if(target > matrix[midRow][m-1])
+                startRow = midRow + 1;
+            else
+                endRow = midRow - 1;
+        }
+
+        return false;
+    }
 }
