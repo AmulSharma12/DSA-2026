6cca4b9405a7a0fb9afbd1eed0caaf4496138f6a
Amul Op <amulsharmarr67@gmail.com>
2026-05-30 00:53:49 +0530

    BS P025 LC240 Search 2d matrix 2 - using optimized appproach

--- DSA-InterviewPrep/src/S006_BinarySearch/P025_LC240_Search2DMatrix2.java
+++ DSA-InterviewPrep/src/S006_BinarySearch/P025_LC240_Search2DMatrix2.java
@@ -18,4 +18,25 @@ public class P025_LC240_Search2DMatrix2 {
 
         return false;
     }
+
+    //Approach 2 - using optimized approach
+    public static boolean searchMatrixUsingOptimized(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        for(int row = 0; row < m; row++){
+            int low = 0;
+            int high = n-1;
+
+            while(low <= high){
+                int mid = low + (high - low)/2;
+                int currElement = matrix[row][mid];
+                if(currElement == target) return true;
+                else if(target > currElement) low = mid + 1;
+                else high = mid - 1;
+            }
+        }
+
+        return false;
+    }
 }
