ea2462f905dd97f3a3bb23b496afaf78f2f543de
Amul Op <amulsharmarr67@gmail.com>
2026-06-01 01:21:54 +0530

    BS P026 LC1901 peak element 2 - using binary search approach

--- DSA-InterviewPrep/src/S006_BinarySearch/P026_LC1901_PeakElement2.java
+++ DSA-InterviewPrep/src/S006_BinarySearch/P026_LC1901_PeakElement2.java
@@ -24,4 +24,44 @@ public class P026_LC1901_PeakElement2 {
 
         return new int[] {-1,-1};
     }
+
+
+    //Approach 2 - using binary search approach
+    public static int[] findPeakGridUsingBinarySearch(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+
+        int low = 0;
+        int high = n-1;
+
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            int maxRowInd = getMax(mat, m, n, mid);
+            int currElement = mat[maxRowInd][mid];
+            int leftElement = mid-1 >= 0 ? mat[maxRowInd][mid-1] : -1;
+            int rightElement = mid + 1 < n ? mat[maxRowInd][mid+1] : -1;
+
+            if(currElement > leftElement && currElement > rightElement)
+                return new int[]{maxRowInd, mid};
+            else if(leftElement > currElement)
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+
+        return new int[]{-1, -1};
+    }
+
+    private static int getMax(int[][] mat, int m, int n, int col){
+        int maxElement = Integer.MIN_VALUE;
+        int maxInd = -1;
+        for(int row = 0; row < m; row++){
+            if(maxElement < mat[row][col]){
+                maxElement = mat[row][col];
+                maxInd = row;
+            }
+        }
+
+        return maxInd;
+    }
 }
