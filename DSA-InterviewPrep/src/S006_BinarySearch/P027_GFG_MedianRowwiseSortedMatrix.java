1e7dacd0b21b725f9a885d430cd0eb1056901813
Amul Op <amulsharmarr67@gmail.com>
2026-06-01 17:50:27 +0530

    BS P027 GFG median row wise sorted matrix - binary search approach

--- DSA-InterviewPrep/src/S006_BinarySearch/P027_GFG_MedianRowwiseSortedMatrix.java
+++ DSA-InterviewPrep/src/S006_BinarySearch/P027_GFG_MedianRowwiseSortedMatrix.java
@@ -24,4 +24,66 @@ public class P027_GFG_MedianRowwiseSortedMatrix {
         Arrays.sort(result);
         return result[x/2];
     }
+
+
+    //Approach 2 - using binary search approach
+
+    private static int upperBound(int[] row, int target) {
+
+        int low = 0;
+        int high = row.length - 1;
+        int ans = row.length;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (row[mid] > target) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
+    private static int countSmallThanMid(int[][] mat, int n, int m, int x){
+        int cnt = 0;
+
+        for(int row = 0; row < n; row++){
+            cnt += upperBound(mat[row], x);
+        }
+
+        return cnt;
+    }
+
+    public static int medianUsingBinarySearchApproach(int[][] mat) {
+        int n = mat.length;
+        int m = mat[0].length;
+
+        int low = Integer.MAX_VALUE;
+        int high = Integer.MIN_VALUE;
+
+        for(int row = 0; row < n; row++){
+            low = Math.min(low, mat[row][0]);
+            high = Math.max(high, mat[row][m-1]);
+        }
+
+        int required = (n*m)/2;
+
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            int smallCount = countSmallThanMid(mat, n, m, mid);
+            if(smallCount <= required)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+
+
+        return low;
+
+    }
 }
