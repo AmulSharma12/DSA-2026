31414a3bfc9743b4ec52bbf5c9125b7ece193ea4
Amul Op <amulsharmarr67@gmail.com>
2026-06-20 16:34:52 +0530

    Tree P021 LC199 right view BT - recursive approach

--- DSA-InterviewPrep/src/S007_Tree/P021_LC199_RightViewBT.java
+++ DSA-InterviewPrep/src/S007_Tree/P021_LC199_RightViewBT.java
@@ -50,4 +50,24 @@ public class P021_LC199_RightViewBT {
 
         return rightSideView;
     }
+
+
+
+    //Approach 2 - using recursive way
+    public static List<Integer> rightSideViewRecursiveApproach(TreeNode root) {
+        List<Integer> rightSideView = new ArrayList<>();
+        if(root == null)    return rightSideView;
+
+        rsvHelper(root, 0, rightSideView);
+        return rightSideView;
+    }
+
+
+    private static void rsvHelper(TreeNode node, int level, List<Integer> rightSideView){
+        if(node == null)    return;
+        if(level == rightSideView.size())   rightSideView.add(node.val);
+
+        rsvHelper(node.right, level+1, rightSideView);
+        rsvHelper(node.left, level+1, rightSideView);
+    }
 }
