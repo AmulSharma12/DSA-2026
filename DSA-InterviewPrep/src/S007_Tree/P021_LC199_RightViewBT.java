2deedbd9f55d9e098de4f458719d2832adcb199d
Amul Op <amulsharmarr67@gmail.com>
2026-06-20 15:34:11 +0530

    Tree P021 LC199 right side view of BT

---
+++ DSA-InterviewPrep/src/S007_Tree/P021_LC199_RightViewBT.java
@@ -0,0 +1,53 @@
+package S007_Tree;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.HashMap;
+import java.util.LinkedList;
+
+//https://leetcode.com/problems/binary-tree-right-side-view/description/
+
+public class P021_LC199_RightViewBT {
+    public static void main(String[] args){
+        //call from here...
+    }
+
+    static class Pair{
+        TreeNode node;
+        int level;
+
+        public Pair(TreeNode node, int level){
+            this.node = node;
+            this.level = level;
+        }
+    }
+
+    public static List<Integer> rightSideView(TreeNode root) {
+        List<Integer> rightSideView = new ArrayList<>();
+        if(root == null)    return rightSideView;
+        Queue<Pair> queue = new LinkedList<>();
+        queue.offer(new Pair(root, 0));
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        while(!queue.isEmpty()){
+            Pair pair = queue.poll();
+            TreeNode node = pair.node;
+            int level = pair.level;
+
+            map.put(level, node.val);
+
+            if(node.left != null)
+                queue.offer(new Pair(node.left, level+1));
+            if(node.right != null)
+                queue.offer(new Pair(node.right, level+1));
+        }
+
+
+        //trvaersing map
+        for(Integer val: map.values()){
+            rightSideView.add(val);
+        }
+
+        return rightSideView;
+    }
+}
