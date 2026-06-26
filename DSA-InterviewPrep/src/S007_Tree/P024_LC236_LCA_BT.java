82b0d478a7bd365c7f647f9e047ccfc3286ef4b8
Amul Op <amulsharmarr67@gmail.com>
2026-06-26 23:19:06 +0530

    P024 LC236 Lowest common ancestor - BT brute force solution

---
+++ DSA-InterviewPrep/src/S007_Tree/P024_LC236_LCA_BT.java
@@ -0,0 +1,76 @@
+package S007_Tree;
+import java.util.List;
+import java.util.ArrayList;
+//https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/description/
+
+public class P024_LC236_LCA_BT {
+    public static void main(String[] args){
+        //call from here...
+    }
+
+    //Approach 1 - using root to node path and storing both path in separte datastructure and returning the last matched node
+    public static TreeNode lowestCommonAncestorBrute(TreeNode root, TreeNode p, TreeNode q) {
+        List<TreeNode> pNodePath = new ArrayList<>();
+        List<TreeNode> qNodePath = new ArrayList<>();
+
+        generatePath(root, p, pNodePath);
+        generatePath(root, q, qNodePath);
+
+        TreeNode lca = root;
+        int psize = pNodePath.size();
+        int qsize = qNodePath.size();
+        int pInd = 0;
+        int qInd = 0;
+
+
+        while(pInd < psize ){
+            System.out.print(pNodePath.get(pInd).val + " ");
+
+            pInd++;
+
+        }
+
+        System.out.println();
+        while(qInd < qsize){
+
+            System.out.print(qNodePath.get(qInd).val + " ");
+
+            qInd++;
+        }
+
+        pInd = 0;
+        qInd = 0;
+
+        while(pInd < psize && qInd < qsize){
+            if(pNodePath.get(pInd).val != qNodePath.get(qInd).val)  break;
+            lca = pNodePath.get(pInd);
+            pInd++;
+            qInd++;
+        }
+
+
+        return lca;
+    }
+
+
+    private static boolean generatePath(TreeNode root, TreeNode target, List<TreeNode> path) {
+
+        if (root == null)
+            return false;
+
+        path.add(root);
+
+        if (root == target)
+            return true;
+
+        if (generatePath(root.left, target, path))
+            return true;
+
+        if (generatePath(root.right, target, path))
+            return true;
+
+        path.remove(path.size() - 1);
+
+        return false;
+    }
+}
